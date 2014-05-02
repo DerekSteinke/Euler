1acac00c2e33a4c4efeadae2662103c034500b8a
DerekSteinke <derek.steinke@gmail.com>
2014-05-02 12:07:15 -0400

    Create P63.java

---
+++ P63.java
@@ -0,0 +1,28 @@
+import java.math.BigInteger;
+
+/**
+ * Finds the number of n-digit positive integer which are also numbers to the
+ * nth power
+ * 
+ * @author derek.steinke
+ * 
+ */
+
+public class P63 {
+
+	public static void main(String[] args) {
+		long start = System.nanoTime();
+		int count = 0;
+		for (int i = 1; i < 15; i++) {
+			for (int j = 1; j < 100; j++) {
+				if (BigInteger.valueOf(i).pow(j).toString().length() == j) {
+					count += 1;
+				}
+			}
+		}
+		System.out.println(count);
+		System.out.println("Done in " + (double) (System.nanoTime() - start)
+				/ 1000000000 + " seconds.");
+
+	}
+}
